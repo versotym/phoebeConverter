e650ef9e848f767ad8f50ebf5202cc19de837e7b
Petr Plechac <plechac@ucl.cas.cz>
2017-10-15 22:09:38 +0200

    Changed indentation

--- phoebeConverter.php
+++ phoebeConverter.php
@@ -2,33 +2,33 @@
 
 class PhoebeConverter {
 
-	public function __construct() {
-		$map_list = json_decode(file_get_contents("map/map.json"), true);
-		$this->map = array(
-			'ipa'    => array(),
-			'xsampa' => array(),
-			'cft'    => array(),
-		);
-		foreach ($map_list as $r){
-			$this->map['xsampa'][$r[0]] = $r[1];
-			$this->map['ipa'][$r[0]] = $r[2];
-			$this->map['cft'][$r[0]] = $r[3];
-		}
-	}
+    public function __construct() {
+        $map_list = json_decode(file_get_contents("map/map.json"), true);
+        $this->map = array(
+            'ipa'    => array(),
+            'xsampa' => array(),
+            'cft'    => array(),
+        );
+        foreach ($map_list as $r){
+            $this->map['xsampa'][$r[0]] = $r[1];
+            $this->map['ipa'][$r[0]] = $r[2];
+            $this->map['cft'][$r[0]] = $r[3];
+        }
+    }
 
-	public function convert($string, $system){
-		if (!in_array($system, array('ipa', 'xsampa', 'cft'))){
-			throw new InvalidArgumentException("<system> must be: 'ipa' | 'xsampa' | 'cft'");
-		}
+    public function convert($string, $system){
+        if (!in_array($system, array('ipa', 'xsampa', 'cft'))){
+            throw new InvalidArgumentException("<system> must be: 'ipa' | 'xsampa' | 'cft'");
+        }
 
-		$converted = '';
-		$snd_array = str_split($string);
+        $converted = '';
+        $snd_array = str_split($string);
 
-		foreach ($snd_array as $i){
-			$converted .= $this->map[$system][$i];
-		}
-		return $converted;
-	}
+        foreach ($snd_array as $i){
+            $converted .= $this->map[$system][$i];
+        }
+        return $converted;
+    }
 
 }
 
